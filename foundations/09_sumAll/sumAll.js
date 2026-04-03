13b8ab1b81bebfab3d6e17ea7d000f3d8329361d
xand144 <174009470+xand144@users.noreply.github.com>
2026-04-03 16:14:49 +0100

    Complete foundations exercise 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!(a === +a) || !(b === +b)) return "ERROR";
+    if (!Number.isInteger(a || b)) return "ERROR";
+    if ((a || b) < 0) return "ERROR";
 
+    let highNum = a > b ? a : b;
+    let lowNum = a < b ? a : b;
+    let result = 0;
+
+    for (let i = lowNum; i <= highNum; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
