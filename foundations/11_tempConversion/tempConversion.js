92d345cf0516677f63e47d34bb0df5fc0a7ddabd
xand144 <174009470+xand144@users.noreply.github.com>
2026-04-04 17:34:13 +0100

    Complete foundations exercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFarenheit) {
+  let result = (tempFarenheit - 32) * 5/9;
+  return Math.round(result * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  let result = tempCelsius * 9 / 5 + 32;
+  return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
